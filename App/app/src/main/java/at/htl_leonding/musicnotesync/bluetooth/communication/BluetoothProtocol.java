5e37ad6bfaf2c60fa256c8cf12a483a63a4cd5dd
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2016-09-04 23:30:05 +0200

    Worked on sending files

--- App/app/src/main/java/at/htl_leonding/musicnotesync/bluetooth/communication/BluetoothProtocol.java
+++ App/app/src/main/java/at/htl_leonding/musicnotesync/bluetooth/communication/BluetoothProtocol.java
@@ -1,9 +1,15 @@
 package at.htl_leonding.musicnotesync.bluetooth.communication;
 
+import java.nio.ByteBuffer;
+
 /**
  * Created by michael on 30.08.16.
  * A mixture of rfcomm and propertiery commands
  */
 public enum BluetoothProtocol {
-    DTR, DSR, DCD, RTS, CTS, TXD, ENQ, ACK, NAK, SOH, EOT, OPF,
+    DTR, DSR, DCD, RTS, CTS, TXD, ENQ, ACK, NAK, SOH, EOT, OPF;
+
+    public static byte[] toByteArray(BluetoothProtocol command){
+        return ByteBuffer.allocate(4).putInt(command.ordinal()).array();
+    }
 }
---
+++ App/app/src/main/java/at/htl_leonding/musicnotesync/global/Constant.java
@@ -0,0 +1,8 @@
+package at.htl_leonding.musicnotesync.global;
+
+/**
+ * Created by michael on 02.09.16.
+ */
+public class Constant {
+    public static final int TRY_MAX = 10;
+}
