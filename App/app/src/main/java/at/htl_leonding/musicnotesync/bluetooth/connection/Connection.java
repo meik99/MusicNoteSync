c97a0b05e0324fd386e9f56357aff28330f6f4d5
meik99 <michaelrynkiewicz3@gmail.com>
2016-10-19 20:08:37 +0200

    Rewrote client Working on reading from blt-inputstream

--- App/app/src/main/java/at/htl_leonding/musicnotesync/bluetooth/connection/Connection.java
+++ App/app/src/main/java/at/htl_leonding/musicnotesync/bluetooth/connection/Connection.java
@@ -2,23 +2,83 @@ package at.htl_leonding.musicnotesync.bluetooth.connection;
 
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.os.AsyncTask;
 import android.util.Base64;
+import android.util.Base64InputStream;
 import android.util.Base64OutputStream;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by michael on 19.09.16.
  */
 
-public class Connection{
+public class Connection implements Runnable{
+
+    public interface ConnectionListener{
+        void onMessageReceived(Connection connection, String message);
+        void onConnectionClosed(Connection connection);
+    }
+
+    @Override
+    public void run() {
+        while(mSocket != null) {
+            InputStream is = null;
+            try {
+                is = mSocket.getInputStream();
+            } catch (IOException e) {
+                Log.i(TAG, "run: " + e.getMessage());
+                close();
+                connectionClosed();
+            }
+
+            if (is != null) {
+                try {
+                    Base64InputStream base64InputStream = new Base64InputStream(is, Base64.DEFAULT);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(
+                            base64InputStream
+                    ));
+                    StringBuilder message = new StringBuilder();
+                    String line = null;
+                    is.read();
+
+                    while ((line = reader.readLine()) != null) {
+                        Log.d(TAG, "run: Received line: \n\t" + line);
+                        message.append(line);
+                    }
+
+                    receivedMessage(message.toString());
+                } catch (IOException e) {
+                    Log.i(TAG, "run: " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    public void close() {
+        if(mSocket != null){
+            try {
+                mSocket.close();
+            } catch (IOException e) {
+                Log.i(TAG, "close: " + e.getMessage());
+            }
+            mSocket = null;
+        }
+    }
+
     private static final String TAG = Connection.class.getSimpleName();
 
-    BluetoothSocket mSocket;
-    BluetoothDevice mDevice;
+    private BluetoothSocket mSocket;
+    private BluetoothDevice mDevice;
+    private List<ConnectionListener> mListener;
 
     public Connection(BluetoothSocket socket){
         if(socket == null)
@@ -26,6 +86,7 @@ public class Connection{
 
         mSocket = socket;
         mDevice = mSocket.getRemoteDevice();
+        mListener = new ArrayList<>();
     }
 
     @Override
@@ -44,20 +105,51 @@ public class Connection{
                                 .getAddress());
     }
 
-    public void sendData(String message) {
-        OutputStream os = null;
+    public void sendData(final String message) {
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                OutputStream os = null;
 
-        try {
-            os = mSocket.getOutputStream();
-        } catch (IOException e) {
-            Log.i(TAG, "sendData: " + e.getMessage());
+                try {
+                    os = mSocket.getOutputStream();
+                } catch (IOException e) {
+                    Log.i(TAG, "sendData: " + e.getMessage());
+                }
+
+                if(os != null){
+                    PrintStream printStream = new PrintStream(
+                            new Base64OutputStream(os, Base64.DEFAULT)
+                    );
+                    printStream.print(message);
+                }
+                return null;
+            }
+        };
+        task.execute();
+    }
+
+    public void addListener(ConnectionListener connectionListener){
+        if(connectionListener != null){
+            mListener.add(connectionListener);
         }
+    }
 
-        if(os != null){
-            PrintStream printStream = new PrintStream(
-                    new Base64OutputStream(os, Base64.DEFAULT)
-            );
-            printStream.print(message);
+    public void removeListener(ConnectionListener connectionListener){
+        if(connectionListener != null){
+            mListener.remove(connectionListener);
+        }
+    }
+
+    private void receivedMessage(String message){
+        for(ConnectionListener listener : mListener){
+            listener.onMessageReceived(this, message);
+        }
+    }
+
+    private void connectionClosed(){
+        for(ConnectionListener listener : mListener) {
+            listener.onConnectionClosed(this);
         }
     }
 }
