e0ee4dbe42974aa600dba2e8c45b3d74c9ffeb46
Hannes <hannes.mitterhuber@aon.at>
2016-11-03 18:27:55 +0100

    Added Folder-entries into RecyclerView in MainActivity. Added GUI to add Folders.

---
+++ App/app/src/main/java/at/htl_leonding/musicnotesync/NameFolderActivity.java
@@ -0,0 +1,13 @@
+package at.htl_leonding.musicnotesync;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NameFolderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_folder);
+    }
+}
