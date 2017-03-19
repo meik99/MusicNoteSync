372d0b20941e4d8141149f8a030699cc0ce87fdb
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2017-03-19 19:42:05 +0100

    Added sending zoom on every zoom action

--- App/app/src/main/java/at/htl_leonding/musicnotesync/presentation/control/zoom/TouchImageViewZoomListener.java
+++ App/app/src/main/java/at/htl_leonding/musicnotesync/presentation/control/zoom/TouchImageViewZoomListener.java
@@ -30,7 +30,38 @@ public class TouchImageViewZoomListener implements ZoomListener {
 
     @Override
     public void onZoom(final TouchImageView view) {
+        final StringBuilder builder = new StringBuilder();
 
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                builder.append(ImageViewController.ZOOM)
+                        .append(";")
+                        .append(view.getCurrentZoom())
+                        .append(";")
+                        .append(view.getZoomedRect().centerX())
+                        .append(";")
+                        .append(view.getZoomedRect().centerY())
+                        .append(";")
+                        .append(view.getScaleType().name());
+
+                BltRepository.getInstance().sendMessage(builder.toString());
+//                AsyncTask task = new AsyncTask() {
+//                    @Override
+//                    protected Object doInBackground(Object[] params) {
+//                        for (BluetoothDevice device :
+//                                mBluetoothDevices) {
+//                            Client client = new Client();
+//                            client.connect(device);
+//                            client.sendMessage(builder.toString());
+//                        }
+//
+//                        return null;
+//                    }
+//                };
+//                task.execute();
+            }
+        });
     }
 
     @Override
